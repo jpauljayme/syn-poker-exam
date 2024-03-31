4035762e5a4d457f401b3b99effece79aec2a0a5
jpauljayme <jpaul.jayme@gmail.com>
2024-03-31 15:52:25 +0800

    Implemented one pair and two pair.

--- src/main/java/com/synacy/poker/hand/HandIdentifier.java
+++ src/main/java/com/synacy/poker/hand/HandIdentifier.java
@@ -3,10 +3,12 @@ package com.synacy.poker.hand;
 import com.synacy.poker.card.Card;
 import com.synacy.poker.card.CardRank;
 import com.synacy.poker.hand.types.OnePair;
+import com.synacy.poker.hand.types.TwoPair;
 import com.synacy.poker.util.CardRankOrderUtil;
 import org.springframework.stereotype.Component;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * A service that is used to identify the {@link Hand} given the player's cards and the community
@@ -25,45 +27,67 @@ public class HandIdentifier {
     public Hand identifyHand(List<Card> playerCards, List<Card> communityCards) {
 
         //TODO: Implement
-        if(!playerCards.isEmpty()){
-
+        if(!playerCards.isEmpty() && !communityCards.isEmpty()){
             List<Card> sortedList = new ArrayList<>(communityCards);
             CardRankOrderUtil
                     .sortCardsDescending(sortedList);
             List<Card> view = Collections.unmodifiableList(sortedList);
 
+            //Put all cards into one stack
+            List<Card> allCards = new ArrayList<>(playerCards);
+            allCards.addAll(communityCards);
+
             Card playerCardOne = playerCards.get(0);
             Card playerCardTwo = playerCards.get(1);
 
             Map<CardRank, Integer> rankCounts = new HashMap<>();
-            for (Card card : communityCards) {
+            for (Card card : allCards) {
                 CardRank rank = card.getRank();
                 rankCounts.put(rank,
                         rankCounts.getOrDefault(rank,
                                 0) + 1);
             }
-            boolean hasOnePairInPlayerCards = hasOnePair(playerCardOne,
-                    playerCardTwo);
 
-            boolean hasPairInCommunityCards =
+            List<CardRank> pairs = countFrequencyOfPairs(rankCounts);
 
-            if(hasOnePairInPlayerCards){
-                //We found a pair
-                System.out.println("asd");
-                List<Card> kickers = Arrays.asList(view.get(0),
-                        view.get(1),
-                        view.get(2));
+            //We found pair/pairs
+            if(!pairs.isEmpty()){
 
-                return new OnePair(playerCards,kickers);
+                //ONE PAIR
+              if(pairs.size() == 1){
+                  List<Card> kickers = Arrays.asList(view.get(0),
+                          view.get(1),
+                          view.get(2));
+
+                  return new OnePair(playerCards,kickers);
+              }else {
+
+                  List<Card> firstPair = allCards.stream().
+                          filter(card -> card.getRank() == pairs.get(0))
+                          .collect(Collectors.toList());
+                  List<Card> secondPair = allCards.stream().
+                          filter(card -> card.getRank() == pairs.get(1))
+                          .collect(Collectors.toList());
+                  return new TwoPair(firstPair,
+                          secondPair,
+                          view.get(0));
+              }
             }
         }
-
-
         return null;
     }
-    private boolean hasOnePair(Card playerCardOne, Card playerCardTwo){
-        return playerCardOne.getRank()
-                .equals(playerCardTwo.getRank());
+
+    private List<CardRank> countFrequencyOfPairs(Map<CardRank, Integer> rankCounts){
+        return rankCounts.entrySet().stream()
+                .filter(entry -> entry.getValue() == 2)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
     }
-    private boolean has
+    private List<CardRank> countFrequencyThreeOfAKind(Map<CardRank, Integer> rankCounts){
+        return rankCounts.entrySet().stream()
+                .filter(entry -> entry.getValue() == 3)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+    }
+
 }
